346b9ac91bdedc685ce32983073f96c270235bfc
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-04-27 02:10:17 +0700

    PBI 2 & modal

--- fronted/src/views/TodoList.vue
+++ fronted/src/views/TodoList.vue
@@ -19,6 +19,11 @@ const selectTodo = (todoId) => {
   selectedTodoId.value = todoId
 }
 
+
+function checkStatus(status){
+    return status.charAt(0).toUpperCase() + status.slice(1).toLowerCase().split('_').join(' ')
+}
+
 const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
 </script>
@@ -46,80 +51,62 @@ const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
   <!-- header -->
   <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6">
-      <h1
-        class="text-3xl font-bold tracking-tight text-gray-900" style="color: #9391e4">
-        IT-Bangmod Kradan Kanban TimeZone : {{ TimeZone }}
-      </h1>
-    </div>
-  </header>
+  <div class="mx-auto max-w-7xl px-4 py-6 md:py-8 lg:py-10">
+    <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight text-gray-900" style="color: #9391e4">
+      IT-Bangmod Kradan Kanban TimeZone : {{ TimeZone }}
+    </h1>
+  </div>
+</header>
   <!-- header --> 
 
   <div class="flex flex-col items-center mt-1" >
     <div class="overflow-x-auto">
-      <table class="table mt-10 rounded-xl overflow-hidden">
-        <!-- head -->
-        <thead>
-          <tr class="bg-base-200 mt-4" style="background-color: #9FC3E9;">
-            <th class="hidden md:table-cell px-4 py-2 text-center text-sm font-semibold text-gray-700">No.</th>
-            <th class="px-4 py-2 text-center text-sm font-semibold text-gray-700">Title</th>
-            <th class="px-4 py-2 text-center text-sm font-semibold text-gray-700">Assignees</th>
-            <th class="px-4 py-2 text-center text-sm font-semibold text-gray-700">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- Iterate over todoList -->
-          <TaskDetail :todo-id="selectedTodoId" />
-          <tr v-for="(item, index) in todoList" :key="index">
-            <th>{{ item.id }}</th>
-            <label
-              for="my_modal_6"
-              class="btn"
-              @click="() => selectTodo(item.id)"
-            >
-              <td>{{ item.title }}</td>
+  <div class="min-w-full">
+    <table class="table-auto mt-10 rounded-xl overflow-hidden">
+      <!-- head -->
+      <thead>
+        <tr class="bg-base-200 mt-4 md:mt-0" style="background-color: #9FC3E9;">
+          <th class="hidden md:table-cell px-4 py-2 text-center md:text-left text-sm font-semibold text-gray-700">No.</th>
+          <th class="px-4 py-2 text-center md:text-left text-sm font-semibold text-gray-700">Title</th>
+          <th class="px-4 py-2 text-center md:text-left text-sm font-semibold text-gray-700">Assignees</th>
+          <th class="px-4 py-2 text-center md:text-left text-sm font-semibold text-gray-700">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Iterate over todoList -->
+        <TaskDetail :todo-id="selectedTodoId" />
+        <tr v-for="(item, index) in todoList" :key="index">
+          <td class="hidden md:table-cell px-4 py-2 text-center md:text-left text-sm text-gray-700">{{ item.id }}</td>
+          <td class="px-4 py-2 text-center md:text-left text-sm text-gray-700">
+            <label for="my_modal_6" @click="() => selectTodo(item.id)">
+              {{ item.title }}
             </label>
-            <td>
-              <span
-                :class="{
-                  'italic text-gray-500':
-                    item.assignees.length === 0 || item.assignees === null
-                }"
-              >
-                {{
-                  item.assignees.length === 0 || item.assignees === null
-                    ? "Unassignees"
-                    : item.assignees
-                }}
-              </span>
-            </td>
-            <td
-              :class="{
-                  'badge badge-outline border border-solid w-20 text-xs px-2 py-1': true,
-                  'border-blue-500 text-blue-500': item.status === 'NO_STATUS',
-                  'border-red-500 text-red-500': item.status === 'TO_DO',
-                  'border-yellow-500 text-yellow-500': item.status === 'DOING',
-                  'border-green-500 text-green-500': item.status === 'DONE'
-              }"
-            >
-              {{ item.status }}
-            </td>
-          </tr>
-         
-        </tbody>
-        <tbody v-if="todoList.length === 0">
-          <tr>
-            <td
-              colspan="4"
-              width="1000px"
-              class="text-center py-4 text-gray-500"
-            >
-              No task
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+          </td>
+          <td class="px-4 py-2 text-center md:text-left text-sm text-gray-700">
+            <span :class="{'italic text-gray-500': item.assignees.length === 0 || item.assignees === null}">
+              {{ item.assignees.length === 0 || item.assignees === null ? "Unassignees" : item.assignees }}
+            </span>
+          </td>
+          <td class="px-4 py-2 text-center md:text-left text-sm text-gray-700">
+            <span :class="{
+              'badge badge-outline border border-solid w-20 text-xs px-2 py-1': true,
+              'border-blue-500 text-blue-500': item.status === 'NO_STATUS',
+              'border-red-500 text-red-500': item.status === 'TO_DO',
+              'border-yellow-500 text-yellow-500': item.status === 'DOING',
+              'border-green-500 text-green-500': item.status === 'DONE'
+            }">
+              {{ checkStatus(item.status) }}
+            </span>
+          </td>
+        </tr>
+        <tr v-if="todoList.length === 0">
+          <td colspan="4" class="text-center py-4 text-gray-500">No task</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
   </div>
 </template>
 <style scoped></style>
