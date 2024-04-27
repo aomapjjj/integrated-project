0778a88da77ff12a70a4a5106872ad0133491eb8
Panipak Jongsirivanich <panipak.jong@kmutt.ac.th>
2024-04-28 00:19:21 +0700

    Update insertDB.sql

--- database/insertDB.sql
+++ database/insertDB.sql
@@ -1,5 +1,5 @@
 INSERT INTO `kanbanIT`.`tasks` (taskTitle, taskDescription, taskAssignees, taskStatus) VALUES
 ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 'NO_STATUS'),
 ('Repository', '', '', 'TO_DO'),
-('ดาต้าเบส', 'ສ ້າງຖານຂໍມ ູນ', 'あなた、彼、彼女(私では ありません)', 'DOING'),
+('ดาต้าเบส', 'ສ ້າງຖານຂໍມ ູນ', 'あなた、彼、彼女 (私ではありません)', 'DOING'),
 ('_Infrastructure_', '_Setup containers_', 'ไก่งวง กับ เพนกวิน', 'DONE');
