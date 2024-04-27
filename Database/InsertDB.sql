c4aa7f6d5d9d284c0b42308ec6260910f2bdecf4
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-04-27 13:47:27 +0700

    committ

--- Database/InsertDB.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `SJ-3`.`TASKS` (taskTitle, taskDescription, taskAssignees, taskStatus) VALUES
-('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 'NO_STATUS'),
-('Repository', '', '', 'TO_DO'),
-('ดาต้าเบส', 'ສ ້າງຖານຂໍມ ູນ', 'あなた、彼、彼女(私では ありません)', 'DOING'),
-('_Infrastructure_', '_Setup containers_', 'ไก่งวง กับ เพนกวิน', 'DONE');
