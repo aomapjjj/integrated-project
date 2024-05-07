50ecf996221e12180e14b3799bfa9c5ac23ac7f0
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-05-08 01:26:35 +0700

    committ

---
+++ fronted/src/libs/fetchUtils.js
@@ -0,0 +1,102 @@
+URL = import.meta.env.VITE_BASE_URL
+
+async function getItems(url) {
+  try {
+    console.log(URL)
+    const data = await fetch(`${url}`) //GET Method
+    const items = await data.json()
+    return items
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+async function getItemById(id) {
+  try {
+    console.log("id : ", id)
+    const data = await fetch(`${URL}/${id}`)
+    //const item = await data.json()
+    return data
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+
+async function deleteItemById(url, id) {
+  console.log(`${url}/${id}`)
+  try {
+    const res = await fetch(`${url}/${id}`, {
+      method: "DELETE"
+    })
+    return res.status
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+
+async function addItem(url, newItem) {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        ...newItem
+      })
+    })
+    const addedItem = await res.json()
+    return addedItem
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+
+async function editItem(url, id, editItem) {
+  console.log(id)
+  console.log(editItem)
+  try {
+    const res = await fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        ...editItem
+      })
+    })
+    const editedItem = await res.json()
+    return editedItem
+  
+    // return res.status
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+
+async function editFavorite(url, id, editItem) {
+  try {
+    const res = await fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        ...editItem,
+
+        favorite: editItem.favorite
+      })
+    })
+    const editedItem = await res.json()
+    return editedItem
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+export {
+  getItems,
+  getItemById,
+  deleteItemById,
+  addItem,
+  editItem,
+  editFavorite
+}
---
+++ fronted/src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router"
+import TaskList from "@/views/TaskList.vue"
+import ErrorPage from "@/views/ErrorPage.vue"
+import AddTask from "@/views/AddTask.vue"
+
+export const routes = [
+  {
+    path: '/',
+    redirect: { name: "TaskList" },
+  },
+  {
+    path: '/task',
+    name: 'TaskList',
+    component: TaskList,
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: { name: "TaskList" },
+  },
+  {
+    path: '/task/:id',
+    name: 'TaskDetail',
+    component: TaskList,
+  },
+  {
+    path: '/task/error',
+    name: 'ErrorPage',
+    component: ErrorPage,
+  },
+  {
+    path: '/task',
+    name: 'AddTask',
+    component: AddTask,
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
