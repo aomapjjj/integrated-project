a0eeba095bf74b7a65d434080956fbdafcb55678
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-05-13 14:49:15 +0700

    commit

--- fronted/src/views/StatusesList.vue
+++ fronted/src/views/StatusesList.vue
@@ -5,7 +5,7 @@ import {
   getItems,
   addItem,
   editItem,
-  deleteItemById
+  deleteItemById,
 } from "../libs/fetchUtils.js"
 import { checkStatus } from "../libs/checkStatus"
 import { toDate } from "../libs/toDate.js"
@@ -25,7 +25,6 @@ const status = ref({
   createdOn: "",
   updatedOn: ""
 })
-
 
 onMounted(async () => {
   const items = await getItems(import.meta.env.VITE_BASE_URL_STATUS)
@@ -102,7 +101,7 @@ const closeModalEdit = () => {
   modal.close()
 }
 // ----------------------- Edit -----------------------
-const oldValue = ref({});
+const oldValue = ref({})
 // ----------------------- Delete -----------------------
 
 const selectedItemIdToDelete = ref(0)
@@ -117,8 +116,10 @@ const deleteStatus = async (statusId) => {
       statusList.value = statusList.value.filter(
         (status) => status.id !== statusId
       )
+     
     } else {
       console.error(`Failed to delete item with ID ${statusId}`)
+
     }
   } catch (error) {
     console.error(`Error deleting item with ID ${statusId}:`, error)
@@ -145,11 +146,12 @@ const confirmDelete = () => {
   // }, 2300)
 }
 
-const deleteandtran = async (statusId, newID) => {
+const deleteandtrans = async (statusId, newID) => {
   try {
     const status = await deleteItemAndTransfer(
       import.meta.env.VITE_BASE_URL_STATUS,
-      statusId, newID
+      statusId,
+      newID
     )
     if (status === 200) {
       statusList.value = statusList.value.filter(
@@ -165,17 +167,16 @@ const deleteandtran = async (statusId, newID) => {
 
 
 
+
 // ----------------------- Delete -----------------------
 
 const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-
 
 const checkEqual = computed(() => {
   console.log(JSON.stringify(status.value))
   console.log(JSON.stringify(oldValue.value))
   return JSON.stringify(status.value) === JSON.stringify(oldValue.value)
 })
-
 </script>
 
 <template>
@@ -193,8 +194,13 @@ const checkEqual = computed(() => {
 
   <!-- header -->
   <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 md:py-8 lg:py-10 flex justify-between items-center">
-      <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight text-gray-900" style="color: #9391e4">
+    <div
+      class="mx-auto max-w-7xl px-4 py-6 md:py-8 lg:py-10 flex justify-between items-center"
+    >
+      <h1
+        class="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight text-gray-900"
+        style="color: #9391e4"
+      >
         IT-Bangmod Kradan Kanban
       </h1>
       <!-- Add new status -->
@@ -202,35 +208,64 @@ const checkEqual = computed(() => {
 
       <dialog id="my_modal_4" class="modal">
         <div class="modal-box w-full md:w-11/12 max-w-5xl mx-auto">
-          <span class="block text-2xl font-bold leading-6 text-gray-900 mb-1" style="margin: 15px">Add Status</span>
+          <span
+            class="block text-2xl font-bold leading-6 text-gray-900 mb-1"
+            style="margin: 15px"
+            >Add Status</span
+          >
 
           <!-- Modal content -->
           <div class="modal-action flex flex-col justify-between">
             <!-- name -->
-            <div class="modal-content py-4 text-left px-6 flex-grow flex flex-col">
-              <label class="itbkk-title input input-bordered flex items-center gap-2 font-bold ml-4 mb-8">
-                <input type="text" class="grow" placeholder="Enter Your Title" maxlength="100" v-model="status.name" />
+            <div
+              class="modal-content py-4 text-left px-6 flex-grow flex flex-col"
+            >
+              <label
+                class="itbkk-title input input-bordered flex items-center gap-2 font-bold ml-4 mb-8"
+              >
+                <input
+                  type="text"
+                  class="grow"
+                  placeholder="Enter Your Title"
+                  maxlength="100"
+                  v-model="status.name"
+                />
               </label>
               <!-- Description -->
               <label for="description" class="form-control flex-grow ml-4 mb-8">
                 <div class="label">
-                  <span class="block text-lg font-bold leading-6 text-gray-900 mb-1">Description</span>
+                  <span
+                    class="block text-lg font-bold leading-6 text-gray-900 mb-1"
+                    >Description</span
+                  >
                 </div>
-                <textarea id="description" class="itbkk-description textarea textarea-bordered flex-grow w-full"
-                  maxlength="500" rows="4" placeholder="No Description Provided"
-                  v-model="status.description"></textarea>
+                <textarea
+                  id="description"
+                  class="itbkk-description textarea textarea-bordered flex-grow w-full"
+                  maxlength="500"
+                  rows="4"
+                  placeholder="No Description Provided"
+                  v-model="status.description"
+                ></textarea>
               </label>
             </div>
             <!-- Buttons -->
             <div class="flex justify-end">
               <form form @submit.prevent="submitForm" method="dialog">
-                <button type="submit" class="itbkk-button-confirm btn mr-2"
+                <button
+                  type="submit"
+                  class="itbkk-button-confirm btn mr-2"
                   style="flex: 3; margin: 10px; background-color: #f785b1"
-                  :disabled="status.name?.length === 0 || status.name === null">
+                  :disabled="status.name?.length === 0 || status.name === null"
+                >
                   Save
                 </button>
               </form>
-              <button class="itbkk-button-cancel btn" style="margin: 10px" @click="closeModalAdd()">
+              <button
+                class="itbkk-button-cancel btn"
+                style="margin: 10px"
+                @click="closeModalAdd()"
+              >
                 Cancel
               </button>
             </div>
@@ -247,116 +282,192 @@ const checkEqual = computed(() => {
           <ul>
             <li>
               <a @click="$router.go(-1)">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  class="w-4 h-4 stroke-current"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                  ></path>
                 </svg>
                 Home
               </a>
             </li>
             <li>
               <a>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  class="w-4 h-4 stroke-current"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                  ></path>
                 </svg>
                 Task Status
               </a>
             </li>
           </ul>
         </div>
-        <table class="table-auto mt-10 rounded-xl overflow-hidden" style="table-layout: fixed">
+        <table
+          class="table-auto mt-10 rounded-xl overflow-hidden"
+          style="table-layout: fixed"
+        >
           <!-- table -->
           <thead>
             <tr class="bg-base-200 mt-4 md:mt-0">
-              <th class="hidden md:table-cell px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
+              <th
+                class="hidden md:table-cell px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
                 style="
                   background-color: #9fc3e9;
                   border-bottom: 2px solid #9fc3e9;
                   color: #fff;
-                ">
+                "
+              >
                 No.
               </th>
-              <th class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700" style="
+              <th
+                class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
+                style="
                   background-color: #9fc3e9;
                   border-bottom: 2px solid #9fc3e9;
                   color: #fff;
-                ">
+                "
+              >
                 Name
               </th>
-              <th class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700" style="
+              <th
+                class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
+                style="
                   background-color: #9fc3e9;
                   border-bottom: 2px solid #9fc3e9;
                   color: #fff;
-                ">
+                "
+              >
                 Description
               </th>
-              <th class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700" style="
+              <th
+                class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
+                style="
                   background-color: #9fc3e9;
                   border-bottom: 2px solid #9fc3e9;
                   color: #fff;
-                ">
+                "
+              >
                 Action
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="itbkk-item" v-for="(item, index) in statusList" :key="index">
+            <tr
+              class="itbkk-item"
+              v-for="(item, index) in statusList"
+              :key="index"
+            >
               <!-- ID -->
-              <td class="hidden md:table-cell px-4 py-2 text-center md:text-left text-sm text-gray-700">
+              <td
+                class="hidden md:table-cell px-4 py-2 text-center md:text-left text-sm text-gray-700"
+              >
                 {{ item.id }}
               </td>
               <!-- NAME -->
-              <td class="px-4 py-2 text-center md:text-left text-sm text-gray-700 itbkk-title">
+              <td
+                class="px-4 py-2 text-center md:text-left text-sm text-gray-700 itbkk-title"
+              >
                 <label for="my_modal_6" @click="selectStatus(item.id)">
                   {{ item.name }}
                 </label>
               </td>
 
-              <td class="px-4 py-2 text-center md:text-left text-sm text-gray-700 itbkk-assignees">
+              <td
+                class="px-4 py-2 text-center md:text-left text-sm text-gray-700 itbkk-assignees"
+              >
                 <label for="my_modal_6">
                   {{ item.description }}
                 </label>
               </td>
 
               <!-- Edit modal-->
-              <td class="px-4 py-2 text-center md:text-left text-sm text-gray-700 itbkk-status">
+              <td
+                class="px-4 py-2 text-center md:text-left text-sm text-gray-700 itbkk-status"
+              >
                 <button class="btn" @click="openModalToEdit(item.id)">
                   edit
                 </button>
 
                 <dialog id="my_modal_edit" class="modal">
                   <div class="modal-box w-full md:w-11/12 max-w-5xl mx-auto">
-                    <span class="block text-2xl font-bold leading-6 text-gray-900 mb-1" style="margin: 15px">Edit
-                      Status</span>
+                    <span
+                      class="block text-2xl font-bold leading-6 text-gray-900 mb-1"
+                      style="margin: 15px"
+                      >Edit Status</span
+                    >
 
                     <!-- Modal content -->
                     <div class="modal-action flex flex-col justify-between">
-                      <div class="modal-content py-4 text-left px-6 flex-grow flex flex-col">
-                        <label class="itbkk-title input input-bordered flex items-center gap-2 font-bold ml-4 mb-8">
-                          <input type="text" class="grow" placeholder="Enter Your Title" maxlength="100"
-                            v-model="status.name" />
+                      <div
+                        class="modal-content py-4 text-left px-6 flex-grow flex flex-col"
+                      >
+                        <label
+                          class="itbkk-title input input-bordered flex items-center gap-2 font-bold ml-4 mb-8"
+                        >
+                          <input
+                            type="text"
+                            class="grow"
+                            placeholder="Enter Your Title"
+                            maxlength="100"
+                            v-model="status.name"
+                          />
                         </label>
                         <!-- Description -->
-                        <label for="description" class="form-control flex-grow ml-4 mb-8">
+                        <label
+                          for="description"
+                          class="form-control flex-grow ml-4 mb-8"
+                        >
                           <div class="label">
-                            <span class="block text-lg font-bold leading-6 text-gray-900 mb-1">Description</span>
+                            <span
+                              class="block text-lg font-bold leading-6 text-gray-900 mb-1"
+                              >Description</span
+                            >
                           </div>
-                          <textarea id="description"
-                            class="itbkk-description textarea textarea-bordered flex-grow w-full" maxlength="500"
-                            rows="4" placeholder="No Description Provided" v-model="status.description"></textarea>
+                          <textarea
+                            id="description"
+                            class="itbkk-description textarea textarea-bordered flex-grow w-full"
+                            maxlength="500"
+                            rows="4"
+                            placeholder="No Description Provided"
+                            v-model="status.description"
+                          ></textarea>
                         </label>
                         <div style="display: flex; flex-direction: row">
                           <div style="margin-right: 30px">
-                            <span class="block text-lg font-bold leading-6 text-gray-900 mb-1">Time Zone</span>
+                            <span
+                              class="block text-lg font-bold leading-6 text-gray-900 mb-1"
+                              >Time Zone</span
+                            >
                             <label>{{ TimeZone }}</label>
                           </div>
                           <div style="margin-right: 30px">
-                            <span class="block text-lg font-bold leading-6 text-gray-900 mb-1">Created On</span>
+                            <span
+                              class="block text-lg font-bold leading-6 text-gray-900 mb-1"
+                              >Created On</span
+                            >
                             <label>{{ toDate(status.createdOn) }}</label>
                           </div>
                           <div>
-                            <span class="block text-lg font-bold leading-6 text-gray-900 mb-1">Updated On</span>
+                            <span
+                              class="block text-lg font-bold leading-6 text-gray-900 mb-1"
+                              >Updated On</span
+                            >
                             <label>{{ toDate(status.updatedOn) }}</label>
                           </div>
                         </div>
@@ -365,12 +476,23 @@ const checkEqual = computed(() => {
                       <!-- Buttons -->
                       <div class="flex justify-end">
                         <form method="dialog">
-                          <button @click="UpdateStatus" type="submit" class="itbkk-button-confirm btn mr-2"
-                            :disabled="status.name?.length === 0 || status.name === null || checkEqual === true">
+                          <button
+                            @click="UpdateStatus"
+                            type="submit"
+                            class="itbkk-button-confirm btn mr-2"
+                            :disabled="
+                              status.name?.length === 0 ||
+                              status.name === null ||
+                              checkEqual === true
+                            "
+                          >
                             Save
                           </button>
                         </form>
-                        <button class="itbkk-button-cancel btn" @click="closeModalEdit()">
+                        <button
+                          class="itbkk-button-cancel btn"
+                          @click="closeModalEdit()"
+                        >
                           Cancel
                         </button>
                       </div>
@@ -379,7 +501,11 @@ const checkEqual = computed(() => {
                 </dialog>
 
                 <!-- Delete Modal -->
-                <button class="itbkk-button-delete btn" style="margin-left: 10px" @click="openModalToDelete(item.id)">
+                <button
+                  class="itbkk-button-delete btn"
+                  style="margin-left: 10px"
+                  @click="openModalToDelete(item.id)"
+                >
                   Delete
                 </button>
                 <dialog id="my_modal_delete" class="modal">
@@ -391,16 +517,23 @@ const checkEqual = computed(() => {
                       Do you want to delete the task number ?
                     </p>
                     <div class="modal-action">
-                      <button class="itbkk-button-cancel btn" @click="closeModal" style="color: #eb4343">
+                      <button
+                        class="itbkk-button-cancel btn"
+                        @click="closeModal"
+                        style="color: #eb4343"
+                      >
                         Cancel
                       </button>
-                      <button class="itbkk-button-confirm btn bg-green-400" style="color: #fff" @click="confirmDelete">
+                      <button
+                        class="itbkk-button-confirm btn bg-green-400"
+                        style="color: #fff"
+                        @click="confirmDelete"
+                      >
                         Confirm
                       </button>
                     </div>
                   </div>
                 </dialog>
-                
               </td>
             </tr>
             <tr v-if="statusList.length === 0">
@@ -415,6 +548,3 @@ const checkEqual = computed(() => {
   </div>
 </template>
 <style scoped></style>
-
-
-
