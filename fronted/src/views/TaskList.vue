b084c719b8efa7b92c4729c0ae15d851e5babfc0
pearmai <pearmai.wann@kmutt.ac.th>
2024-05-15 03:07:16 +0700

    main

--- fronted/src/views/TaskList.vue
+++ fronted/src/views/TaskList.vue
@@ -1,115 +1,115 @@
 <script setup>
-import { ref, onMounted } from "vue"
-import { getItemById, getItems, deleteItemById } from "../libs/fetchUtils.js"
-import TaskDetail from "../views/TaskDetail.vue"
-import AddTask from "../views/AddTask.vue"
-import EditTask from "../views/EditTask.vue"
-import StatusesList from "../views/StatusesList.vue"
-import { checkStatus } from "../libs/checkStatus"
-import { useRoute, useRouter } from "vue-router"
+import { ref, onMounted } from 'vue';
+import { getItemById, getItems, deleteItemById } from '../libs/fetchUtils.js';
+import TaskDetail from '../views/TaskDetail.vue';
+import AddTask from '../views/AddTask.vue';
+import EditTask from '../views/EditTask.vue';
+import StatusesList from '../views/StatusesList.vue';
+import { checkStatus } from '../libs/checkStatus';
+import { useRoute, useRouter } from 'vue-router';
 
-const route = useRoute()
-const router = useRouter()
-const todoList = ref([])
-const selectedTodoId = ref(0)
-const notFound = ref(false)
-const deleteComplete = ref(false)
-const showDetail = ref(false)
-const statusList = ref([])
-let items = [] // ประกาศ items เป็นตัวแปร global
-let itemsStatus = [] // ประกาศ items เป็นตัวแปร global
+const route = useRoute();
+const router = useRouter();
+const todoList = ref([]);
+const selectedTodoId = ref(0);
+const notFound = ref(false);
+const deleteComplete = ref(false);
+const showDetail = ref(false);
+const statusList = ref([]);
+let items = []; // ประกาศ items เป็นตัวแปร global
+let itemsStatus = []; // ประกาศ items เป็นตัวแปร global
 
-const baseUrlTask = `${import.meta.env.VITE_BASE_URL_MAIN}/tasks`
-const baseUrlStatus = `${import.meta.env.VITE_BASE_URL_MAIN}/statuses`
+const baseUrlTask = `${import.meta.env.VITE_BASE_URL_MAIN}/tasks`;
+const baseUrlStatus = `${import.meta.env.VITE_BASE_URL_MAIN}/statuses`;
 
 const todo = ref({
-  title: "",
-  description: "",
-  assignees: "",
-  status: "NO_STATUS"
-})
+  title: '',
+  description: '',
+  assignees: '',
+  status: 'NO_STATUS',
+});
 
 onMounted(async () => {
-  items = await getItems(baseUrlTask)
-  itemsStatus = await getItems(baseUrlStatus)
-  statusList.value = itemsStatus
+  items = await getItems(baseUrlTask);
+  itemsStatus = await getItems(baseUrlStatus);
+  statusList.value = itemsStatus;
 
-  console.log("itemStatuss", itemsStatus)
-  todoList.value = items
-  console.log("items", items)
-  const taskId = route.params.id
+  console.log('itemStatuss', itemsStatus);
+  todoList.value = items;
+  console.log('items', items);
+  const taskId = route.params.id;
 
   if (taskId !== undefined) {
-    console.log(taskId)
-    const response = await getItemById(taskId)
+    console.log(taskId);
+    const response = await getItemById(taskId);
     if (response.status === 404 || response.status === 400) {
-      router.push("/task/error")
-      notFound.value = true
+      router.push('/task/error');
+      notFound.value = true;
     }
   }
-  return items
-})
+  return items;
+});
 
 const selectTodo = (todoId) => {
-  selectedTodoId.value = todoId
-  showDetail.value = true
-}
+  selectedTodoId.value = todoId;
+  showDetail.value = true;
+};
 
 // ----------------------- Delete -----------------------
 
-const selectedItemIdToDelete = ref(0)
+const selectedItemIdToDelete = ref(0);
 
 const deleteTodo = async (todoId) => {
   try {
-    const status = await deleteItemById(baseUrlTask, todoId)
+    const status = await deleteItemById(baseUrlTask, todoId);
     if (status === 200) {
-      todoList.value = todoList.value.filter((todo) => todo.id !== todoId)
+      todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
     } else {
-      console.error(`Failed to delete item with ID ${todoId}`)
+      console.error(`Failed to delete item with ID ${todoId}`);
     }
   } catch (error) {
-    console.error(`Error deleting item with ID ${todoId}:`, error)
+    console.error(`Error deleting item with ID ${todoId}:`, error);
   }
-}
+};
 
 const openModalToDelete = (itemId) => {
-  selectedItemIdToDelete.value = itemId
-  const modal = document.getElementById("my_modal_delete")
-  modal.showModal()
-}
+  selectedItemIdToDelete.value = itemId;
+  const modal = document.getElementById('my_modal_delete');
+  modal.showModal();
+};
 
 const closeModal = () => {
-  const modal = document.getElementById("my_modal_delete")
-  modal.close()
-}
+  const modal = document.getElementById('my_modal_delete');
+  modal.close();
+};
 
 const confirmDelete = () => {
-  deleteTodo(selectedItemIdToDelete.value)
-  closeModal()
-  deleteComplete.value = true
+  deleteTodo(selectedItemIdToDelete.value);
+  closeModal();
+  deleteComplete.value = true;
   setTimeout(() => {
-    deleteComplete.value = false
-  }, 2300)
-}
+    deleteComplete.value = false;
+  }, 2300);
+};
 
 // ----------------------- Delete -----------------------
 
 // ----------------------- filterAndLogTitleById -----------------------
 
 const filterAndLogTitleById = (id) => {
-  const item = items.find((item) => item.id === id)
+  const item = items.find((item) => item.id === id);
   if (item) {
-    console.log(item.title)
-    return item.title
+    console.log(item.title);
+    return item.title;
   } else {
-    console.log(`No item found with id ${id}`)
-    return ""
+    console.log(`No item found with id ${id}`);
+    return '';
   }
-}
+};
 
 // ----------------------- filterAndLogTitleById -----------------------
 
-const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 // ----------------------- STATUS SORT -----------------------
 const showIcon = ref('default');
@@ -123,24 +123,24 @@ const toggleIcon = () => {
     showIcon.value = 'asc';
   }
 };
-const statusSortOrder = ref("asc")
+const statusSortOrder = ref('asc');
 
 const sortByStatus = () => {
-  const currentSortOrder = statusSortOrder.value
-  if (currentSortOrder === "asc") {
-    todoList.value.sort((a, b) => a.status.localeCompare(b.status))
-    statusSortOrder.value = "desc"
+  const currentSortOrder = statusSortOrder.value;
+  if (currentSortOrder === 'asc') {
+    todoList.value.sort((a, b) => a.status.localeCompare(b.status));
+    statusSortOrder.value = 'desc';
   } else {
-    todoList.value.sort((a, b) => b.status.localeCompare(a.status))
-    statusSortOrder.value = "asc"
+    todoList.value.sort((a, b) => b.status.localeCompare(a.status));
+    statusSortOrder.value = 'asc';
   }
-}
+};
 
 // ----------------------- STATUS SORT -----------------------
 
 const openNewStatus = () => {
-  router.push({ name: "StatusesList" })
-}
+  router.push({ name: 'StatusesList' });
+};
 </script>
 
 <template>
@@ -215,10 +215,7 @@ const openNewStatus = () => {
   <div class="flex flex-col items-center mt-9">
     <div class="overflow-x-auto max-h-96 w-min-full">
       <div class="min-w-full">
-        <table
-          class="table-auto"
-          style="table-layout: fixed;"
-        >
+        <table class="table-auto" style="table-layout: fixed">
           <!-- table -->
           <thead>
             <tr class="bg-base-200 mt-4 md:mt-0">
@@ -270,7 +267,7 @@ const openNewStatus = () => {
                   Status
                   <!-- Default -->
                   <svg
-                  v-if="showIcon === 'default'"
+                    v-if="showIcon === 'default'"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -288,7 +285,7 @@ const openNewStatus = () => {
                   </svg>
                   <!-- Asc -->
                   <svg
-                  v-else-if="showIcon === 'asc'"
+                    v-else-if="showIcon === 'asc'"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -299,14 +296,15 @@ const openNewStatus = () => {
                         d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
                       />
                       <path
-                        fill="red"
+                        fill="#eb4343"
                         d="M10.759 13c.94 0 1.43 1.092.855 1.792l-.078.086L7.414 19H11a1 1 0 0 1 .117 1.993L11 21H5.241c-.94 0-1.43-1.092-.855-1.792l.078-.086L8.586 15H5a1 1 0 0 1-.117-1.993L5 13zM17 4a1 1 0 0 1 1 1v12.414l1.121-1.121a1 1 0 0 1 1.415 1.414l-2.829 2.828a1 1 0 0 1-1.414 0l-2.828-2.828a1 1 0 0 1 1.414-1.414L16 17.414V5a1 1 0 0 1 1-1M8 3c.674 0 1.28.396 1.556 1.002l.054.133l2.332 6.529a1 1 0 0 1-1.838.78l-.046-.108L9.581 10H6.419l-.477 1.336a1 1 0 0 1-1.917-.56l.033-.112l2.332-6.53A1.71 1.71 0 0 1 8 3m0 2.573L7.133 8h1.734z"
                       />
                     </g>
                   </svg>
 
                   <!-- Des -->
-                  <svg v-else
+                  <svg
+                    v-else
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -317,7 +315,7 @@ const openNewStatus = () => {
                         d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"
                       />
                       <path
-                        fill="blue"
+                        fill="#4361ee"
                         d="M4.664 11.942a1 1 0 0 0 1.278-.606L6.419 10h3.162l.477 1.336a1 1 0 0 0 1.884-.672L9.61 4.134a1.71 1.71 0 0 0-3.22 0l-2.332 6.53a1 1 0 0 0 .606 1.278M8 5.573L8.867 8H7.133zm8.293-1.28a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1-1.415 1.415L18 7.414V20a1 1 0 1 1-2 0V7.414l-1.121 1.122a1 1 0 1 1-1.415-1.415zM5 13a1 1 0 1 0 0 2h3.586l-4.122 4.122C3.77 19.815 4.26 21 5.24 21H11a1 1 0 1 0 0-2H7.414l4.122-4.122c.693-.693.203-1.878-.777-1.878z"
                       />
                     </g>
@@ -366,12 +364,12 @@ const openNewStatus = () => {
               <td
                 class="itbkk-assignees px-4 py-2 text-center md:text-left text-sm text-gray-700"
                 :class="{
-                  italic: !item.assignees || item.assignees.length === 0
+                  italic: !item.assignees || item.assignees.length === 0,
                 }"
               >
                 {{
                   !item.assignees || item.assignees.length === 0
-                    ? "Unassigned"
+                    ? 'Unassigned'
                     : item.assignees
                 }}
               </td>
@@ -386,7 +384,7 @@ const openNewStatus = () => {
                     'border-red-500 text-red-500': item.status === 'To Do',
                     'border-yellow-500 text-yellow-500':
                       item.status === 'Doing',
-                    'border-green-500 text-green-500': item.status === 'Done'
+                    'border-green-500 text-green-500': item.status === 'Done',
                   }"
                 >
                   {{ item.status }}
@@ -394,26 +392,69 @@ const openNewStatus = () => {
               </td>
 
               <div class="itbkk-button-action">
-                <td style="display: flex; justify-content: center;">
-                <div class="itbkk-button-edit hidden md:table-cell text-sm px-4 py-2">
-                  <div v-if="index !== 0 && index !== 1"class="dropdown dropdown-top dropdown-end">
-                    <div tabindex="0">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
-                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M12 6h.01M12 12h.01M12 18h.01"/>
-                      </svg>
-                    </div>
-                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <!-- EDIT -->
-                      <li><a><EditTask :todo-id="item.id" /></a></li>
-                    <!-- Delete -->
-                      <li><a style="width: 150px; margin-left: 17px;" class="itbkk-button-delete btn" @click="openModalToDelete(item.id)">Delete</a></li>
-                    </ul>
-                  </div>
+                <td style="display: flex; justify-content: center">
+                  <div
+                    class="itbkk-button-edit hidden md:table-cell text-sm px-4 py-2"
+                  >
+                    <div
+                      v-if="index !== 0 && index !== 1"
+                      class="dropdown dropdown-top dropdown-end"
+                    >
+                      <div tabindex="0">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="28"
+                          height="28"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-width="3"
+                            d="M12 6h.01M12 12h.01M12 18h.01"
+                          />
+                        </svg>
+                      </div>
+                      <ul
+                        tabindex="0"
+                        class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                      >
+                        <!-- EDIT -->
+                        <li>
+                          <a><EditTask :todo-id="item.id" /></a>
+                        </li>
 
-                  <div  v-if="index === 0 || index === 1"  class="dropdown  dropdown-end">
+                        <!-- Delete -->
+                        <li>
+                          <a
+                            style="width: 150px; margin-left: 17px"
+                            class="itbkk-button-delete btn"
+                            @click="openModalToDelete(item.id)"
+                            >Delete</a
+                          >
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div
+                    v-if="index === 0 || index === 1"
+                    class="dropdown dropdown-end"
+                  >
                     <div tabindex="0">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
-                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M12 6h.01M12 12h.01M12 18h.01"/>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28"
+                        height="28"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-width="3"
+                          d="M12 6h.01M12 12h.01M12 18h.01"
+                        />
                       </svg>
                     </div>
 
@@ -506,18 +547,23 @@ const openNewStatus = () => {
     </div>
   </div>
 
-<footer class="footer footer-center p-5 text-primary-content" style="background-color: #f785b1; margin-top: 37px;">
-  <aside class="flex items-center space-x-20">
-    <img src="/src/image/sit.png" alt="SIT logo" style="width: 180px; margin-right: 100px;">    
-    <p class="font-bold mb-2 text-gray-900">
-      IT-Bangmod Kradan Kanban was created by 2nd year undergraduate students
-      <br>( INT221 Integrated Information Technology Project I )
-    </p> 
-    <p class="text-white">Thank you for choosing Kradan Kanban!</p>
-  </aside> 
-</footer>
-
-
+  <footer
+    class="footer footer-center p-5 text-primary-content"
+    style="background-color: #f785b1; margin-top: 37px"
+  >
+    <aside class="flex items-center space-x-20">
+      <img
+        src="/src/image/sit.png"
+        alt="SIT logo"
+        style="width: 180px; margin-right: 100px"
+      />
+      <p class="font-bold mb-2 text-gray-900">
+        IT-Bangmod Kradan Kanban was created by 2nd year undergraduate students
+        <br />( INT221 Integrated Information Technology Project I )
+      </p>
+      <p class="text-white">Thank you for choosing Kradan Kanban!</p>
+    </aside>
+  </footer>
 </template>
 <style scoped>
 #tasktable {
@@ -528,7 +574,7 @@ const openNewStatus = () => {
 table {
   border-collapse: separate;
   border-spacing: 0;
-  border-radius: 10px; 
+  border-radius: 10px;
   overflow: hidden;
 }
 
