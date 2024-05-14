dce7861aab3742a1ea4434d3629018847eb41ba0
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-05-15 00:27:19 +0700

    commit sort eiei

--- fronted/src/views/TaskList.vue
+++ fronted/src/views/TaskList.vue
@@ -19,8 +19,8 @@ const statusList = ref([])
 let items = [] // ประกาศ items เป็นตัวแปร global
 let itemsStatus = [] // ประกาศ items เป็นตัวแปร global
 
-const baseUrlTask = `${import.meta.env.VITE_BASE_URL_MAIN}/tasks`;
-const baseUrlStatus = `${import.meta.env.VITE_BASE_URL_MAIN}/statuses`;
+const baseUrlTask = `${import.meta.env.VITE_BASE_URL_MAIN}/tasks`
+const baseUrlStatus = `${import.meta.env.VITE_BASE_URL_MAIN}/statuses`
 
 const todo = ref({
   title: "",
@@ -33,90 +33,114 @@ onMounted(async () => {
   items = await getItems(baseUrlTask)
   itemsStatus = await getItems(baseUrlStatus)
   statusList.value = itemsStatus
-  
-  console.log('itemStatuss', itemsStatus)
+
+  console.log("itemStatuss", itemsStatus)
   todoList.value = items
-  console.log('items', items)
+  console.log("items", items)
   const taskId = route.params.id
 
   if (taskId !== undefined) {
-    console.log(taskId);
-    const response = await getItemById(taskId);
+    console.log(taskId)
+    const response = await getItemById(taskId)
     if (response.status === 404 || response.status === 400) {
-      router.push('/task/error');
-      notFound.value = true;
+      router.push("/task/error")
+      notFound.value = true
     }
   }
   return items
 })
 
-
 const selectTodo = (todoId) => {
-  selectedTodoId.value = todoId;
-  showDetail.value = true;
-};
-
-
+  selectedTodoId.value = todoId
+  showDetail.value = true
+}
 
 // ----------------------- Delete -----------------------
 
-const selectedItemIdToDelete = ref(0);
+const selectedItemIdToDelete = ref(0)
 
 const deleteTodo = async (todoId) => {
   try {
-    const status = await deleteItemById(baseUrlTask, todoId);
+    const status = await deleteItemById(baseUrlTask, todoId)
     if (status === 200) {
-      todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+      todoList.value = todoList.value.filter((todo) => todo.id !== todoId)
     } else {
-      console.error(`Failed to delete item with ID ${todoId}`);
+      console.error(`Failed to delete item with ID ${todoId}`)
     }
   } catch (error) {
-    console.error(`Error deleting item with ID ${todoId}:`, error);
+    console.error(`Error deleting item with ID ${todoId}:`, error)
   }
-};
+}
 
 const openModalToDelete = (itemId) => {
-  selectedItemIdToDelete.value = itemId;
-  const modal = document.getElementById('my_modal_delete');
-  modal.showModal();
-};
+  selectedItemIdToDelete.value = itemId
+  const modal = document.getElementById("my_modal_delete")
+  modal.showModal()
+}
 
 const closeModal = () => {
-  const modal = document.getElementById('my_modal_delete');
-  modal.close();
-};
+  const modal = document.getElementById("my_modal_delete")
+  modal.close()
+}
 
 const confirmDelete = () => {
-  deleteTodo(selectedItemIdToDelete.value);
-  closeModal();
-  deleteComplete.value = true;
+  deleteTodo(selectedItemIdToDelete.value)
+  closeModal()
+  deleteComplete.value = true
   setTimeout(() => {
-    deleteComplete.value = false;
-  }, 2300);
-};
+    deleteComplete.value = false
+  }, 2300)
+}
 
 // ----------------------- Delete -----------------------
 
 // ----------------------- filterAndLogTitleById -----------------------
 
 const filterAndLogTitleById = (id) => {
-  const item = items.find((item) => item.id === id);
+  const item = items.find((item) => item.id === id)
   if (item) {
-    console.log(item.title);
-    return item.title;
+    console.log(item.title)
+    return item.title
   } else {
-    console.log(`No item found with id ${id}`);
-    return '';
+    console.log(`No item found with id ${id}`)
+    return ""
   }
-};
+}
 
 // ----------------------- filterAndLogTitleById -----------------------
 
-const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+// ----------------------- STATUS SORT -----------------------
+const showIcon = ref('default');
+
+const toggleIcon = () => {
+  if (showIcon.value === 'default') {
+    showIcon.value = 'asc';
+  } else if (showIcon.value === 'asc') {
+    showIcon.value = 'desc';
+  } else {
+    showIcon.value = 'asc';
+  }
+};
+const statusSortOrder = ref("asc")
+
+const sortByStatus = () => {
+  const currentSortOrder = statusSortOrder.value
+  if (currentSortOrder === "asc") {
+    todoList.value.sort((a, b) => a.status.localeCompare(b.status))
+    statusSortOrder.value = "desc"
+  } else {
+    todoList.value.sort((a, b) => b.status.localeCompare(a.status))
+    statusSortOrder.value = "asc"
+  }
+}
+
+// ----------------------- STATUS SORT -----------------------
 
 const openNewStatus = () => {
-  router.push({ name: 'StatusesList' });
-};
+  router.push({ name: "StatusesList" })
+}
 </script>
 
 <template>
@@ -209,10 +233,7 @@ const openNewStatus = () => {
 
         <AddTask :todo="todo" />
 
-        <table
-          class="table-auto mt-10 rounded-xl"
-          style="table-layout: fixed;"
-        >
+        <table class="table-auto mt-10 rounded-xl" style="table-layout: fixed">
           <!-- table -->
           <thead>
             <tr class="bg-base-200 mt-4 md:mt-0">
@@ -246,6 +267,9 @@ const openNewStatus = () => {
               >
                 Assignees
               </th>
+
+              <!-- STATUS SORT -->
+
               <th
                 class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
                 style="
@@ -254,8 +278,70 @@ const openNewStatus = () => {
                   color: #fff;
                 "
               >
-                Status
+                <button
+                  style="display: flex; align-items: center"
+                  @click="sortByStatus(), toggleIcon()"
+                >
+                  Status
+                  <!-- Default -->
+                  <svg
+                  v-if="showIcon === 'default'"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <g fill="none" fill-rule="evenodd">
+                      <path
+                        d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+                      />
+                      <path
+                        fill="currentColor"
+                        d="M10.759 13c.94 0 1.43 1.092.855 1.792l-.078.086L7.414 19H11a1 1 0 0 1 .117 1.993L11 21H5.241c-.94 0-1.43-1.092-.855-1.792l.078-.086L8.586 15H5a1 1 0 0 1-.117-1.993L5 13zM17 4a1 1 0 0 1 1 1v12.414l1.121-1.121a1 1 0 0 1 1.415 1.414l-2.829 2.828a1 1 0 0 1-1.414 0l-2.828-2.828a1 1 0 0 1 1.414-1.414L16 17.414V5a1 1 0 0 1 1-1M8 3c.674 0 1.28.396 1.556 1.002l.054.133l2.332 6.529a1 1 0 0 1-1.838.78l-.046-.108L9.581 10H6.419l-.477 1.336a1 1 0 0 1-1.917-.56l.033-.112l2.332-6.53A1.71 1.71 0 0 1 8 3m0 2.573L7.133 8h1.734z"
+                      />
+                    </g>
+                  </svg>
+                  <!-- Asc -->
+                  <svg
+                  v-else-if="showIcon === 'asc'"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <g fill="none" fill-rule="evenodd">
+                      <path
+                        d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+                      />
+                      <path
+                        fill="red"
+                        d="M10.759 13c.94 0 1.43 1.092.855 1.792l-.078.086L7.414 19H11a1 1 0 0 1 .117 1.993L11 21H5.241c-.94 0-1.43-1.092-.855-1.792l.078-.086L8.586 15H5a1 1 0 0 1-.117-1.993L5 13zM17 4a1 1 0 0 1 1 1v12.414l1.121-1.121a1 1 0 0 1 1.415 1.414l-2.829 2.828a1 1 0 0 1-1.414 0l-2.828-2.828a1 1 0 0 1 1.414-1.414L16 17.414V5a1 1 0 0 1 1-1M8 3c.674 0 1.28.396 1.556 1.002l.054.133l2.332 6.529a1 1 0 0 1-1.838.78l-.046-.108L9.581 10H6.419l-.477 1.336a1 1 0 0 1-1.917-.56l.033-.112l2.332-6.53A1.71 1.71 0 0 1 8 3m0 2.573L7.133 8h1.734z"
+                      />
+                    </g>
+                  </svg>
+
+                  <!-- Des -->
+                  <svg v-else
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <g fill="none" fill-rule="evenodd">
+                      <path
+                        d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"
+                      />
+                      <path
+                        fill="blue"
+                        d="M4.664 11.942a1 1 0 0 0 1.278-.606L6.419 10h3.162l.477 1.336a1 1 0 0 0 1.884-.672L9.61 4.134a1.71 1.71 0 0 0-3.22 0l-2.332 6.53a1 1 0 0 0 .606 1.278M8 5.573L8.867 8H7.133zm8.293-1.28a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1-1.415 1.415L18 7.414V20a1 1 0 1 1-2 0V7.414l-1.121 1.122a1 1 0 1 1-1.415-1.415zM5 13a1 1 0 1 0 0 2h3.586l-4.122 4.122C3.77 19.815 4.26 21 5.24 21H11a1 1 0 1 0 0-2H7.414l4.122-4.122c.693-.693.203-1.878-.777-1.878z"
+                      />
+                    </g>
+                  </svg>
+                </button>
               </th>
+
+              <!-- STATUS SORT -->
+
               <th
                 class="px-4 py-2 text-center md:text-left text-md font-semibold text-gray-700"
                 style="
@@ -295,12 +381,12 @@ const openNewStatus = () => {
               <td
                 class="itbkk-assignees px-4 py-2 text-center md:text-left text-sm text-gray-700"
                 :class="{
-                  italic: !item.assignees || item.assignees.length === 0,
+                  italic: !item.assignees || item.assignees.length === 0
                 }"
               >
                 {{
                   !item.assignees || item.assignees.length === 0
-                    ? 'Unassigned'
+                    ? "Unassigned"
                     : item.assignees
                 }}
               </td>
@@ -315,7 +401,7 @@ const openNewStatus = () => {
                     'border-red-500 text-red-500': item.status === 'To Do',
                     'border-yellow-500 text-yellow-500':
                       item.status === 'Doing',
-                    'border-green-500 text-green-500': item.status === 'Done',
+                    'border-green-500 text-green-500': item.status === 'Done'
                   }"
                 >
                   {{ item.status }}
@@ -323,56 +409,81 @@ const openNewStatus = () => {
               </td>
 
               <div class="itbkk-button-action">
-                <td style="display: flex; justify-content: center;">
-                <div class="itbkk-button-edit hidden md:table-cell text-sm px-4 py-2">
-                  <div class="dropdown dropdown-top dropdown-end">
-                    <div tabindex="0">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
-                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M12 6h.01M12 12h.01M12 18h.01"/>
-                      </svg>
-                    </div>
-                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <!-- EDIT -->
-                      <li><a><EditTask :todo-id="item.id" /></a></li>
-                    <!-- Delete -->
-                      <li><a style="width: 150px; margin-left: 17px;" class="itbkk-button-delete btn" @click="openModalToDelete(item.id)">Delete</a></li>
-                    </ul>
-                  </div>
-
-                  <dialog id="my_modal_delete" class="modal">
-                    <div class="modal-box" style="max-width: 1000px">
-                      <h3 class=" font-bold text-lg">
-                        Delete a Task
-                      </h3>
-                      <p class="itbkk-message py-4 font-medium" style="word-wrap: break-word">
-                        Do you want to delete the task number
-                        {{ selectedItemIdToDelete }} - "{{
-                          filterAndLogTitleById(selectedItemIdToDelete)
-                        }}"?
-                      </p>
-                      <div class="modal-action">
-                        <button
-                          class="itbkk-button-cancel btn "
-                          @click="closeModal"
-                          style="color: #eb4343; pointer-events: none"
+                <td style="display: flex; justify-content: center">
+                  <div
+                    class="itbkk-button-edit hidden md:table-cell text-sm px-4 py-2"
+                  >
+                    <div class="dropdown dropdown-top dropdown-end">
+                      <div tabindex="0">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="28"
+                          height="28"
+                          viewBox="0 0 24 24"
                         >
-                          Cancel
-                        </button>
-                        <button
-                          class="itbkk-button-confirm btn bg-green-400"
-                          style="color: #fff"
-                          @click="confirmDelete"
-                        >
-                          Confirm
-                        </button>
+                          <path
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-width="3"
+                            d="M12 6h.01M12 12h.01M12 18h.01"
+                          />
+                        </svg>
                       </div>
+                      <ul
+                        tabindex="0"
+                        class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                      >
+                        <!-- EDIT -->
+                        <li>
+                          <a><EditTask :todo-id="item.id" /></a>
+                        </li>
+                        <!-- Delete -->
+                        <li>
+                          <a
+                            style="width: 150px; margin-left: 17px"
+                            class="itbkk-button-delete btn"
+                            @click="openModalToDelete(item.id)"
+                            >Delete</a
+                          >
+                        </li>
+                      </ul>
                     </div>
-                  </dialog>
-                </div>
-                <!-- DELETE -->
-              </td>
+
+                    <dialog id="my_modal_delete" class="modal">
+                      <div class="modal-box" style="max-width: 1000px">
+                        <h3 class="font-bold text-lg">Delete a Task</h3>
+                        <p
+                          class="itbkk-message py-4 font-medium"
+                          style="word-wrap: break-word"
+                        >
+                          Do you want to delete the task number
+                          {{ selectedItemIdToDelete }} - "{{
+                            filterAndLogTitleById(selectedItemIdToDelete)
+                          }}"?
+                        </p>
+                        <div class="modal-action">
+                          <button
+                            class="itbkk-button-cancel btn"
+                            @click="closeModal"
+                            style="color: #eb4343; pointer-events: none"
+                          >
+                            Cancel
+                          </button>
+                          <button
+                            class="itbkk-button-confirm btn bg-green-400"
+                            style="color: #fff"
+                            @click="confirmDelete"
+                          >
+                            Confirm
+                          </button>
+                        </div>
+                      </div>
+                    </dialog>
+                  </div>
+                  <!-- DELETE -->
+                </td>
               </div>
-              
             </tr>
 
             <!-- DELETE COMPLETE -->
@@ -413,9 +524,7 @@ const openNewStatus = () => {
           ></path>
         </svg>
         <div>
-          <h2 class="itbkk-message font-bold">
-            The task has been deleted
-          </h2>
+          <h2 class="itbkk-message font-bold">The task has been deleted</h2>
         </div>
         <div>
           <button
@@ -447,5 +556,4 @@ tr:nth-child(odd) {
 thead th {
   height: 3rem; /* Adjust the height as needed */
 }
-
 </style>
