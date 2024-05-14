09a03a48983e58861fa4e854be9b4fc1a6aaddb2
pearmai <pearmai.wann@kmutt.ac.th>
2024-05-14 13:46:34 +0700

    database

--- database/insert_status.sql
+++ database/insert_status.sql
@@ -1,8 +1,15 @@
 -- Status
+
+-- INSERT INTO kanbanIT.statustasks (statusid, statusname, statusdescription) VALUES
+-- (1, 'NO_STATUS', NULL),
+-- (2, 'TO_DO', NULL),
+-- (3, 'DOING', NULL),
+-- (4, 'DONE', NULL);
+
 INSERT INTO kanbanIT.statustasks (statusid, statusname, statusdescription) VALUES
-(1, 'NO_STATUS', NULL),
-(2, 'TO_DO', NULL),
-(3, 'DOING', NULL),
-(4, 'DONE', NULL);
+(1, 'No Status', NULL),
+(2, 'To Do', NULL),
+(3, 'Doing', NULL),
+(4, 'Done', NULL);
 
 COMMIT;
