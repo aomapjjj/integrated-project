6345e1591636c20b5c289ca95970397194b49a45
Panipak Jongsirivanich <panipak.jong@kmutt.ac.th>
2024-05-16 15:57:55 +0700

    Update db-env.sh

--- database/db-env.sh
+++ database/db-env.sh
@@ -1,5 +1,5 @@
 # use source db-env.sh to set variables on current shell
 
 export MYSQL_USER=dev
-export MYSQL_PASSWORD=x_eRT2vv4
-export MYSQL_DATABASE=school
+export MYSQL_PASSWORD=mysql
+export MYSQL_DATABASE=kanbanIT
