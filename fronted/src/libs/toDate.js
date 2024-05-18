b75cf23ecb6a0375d0fe0deee56b31fb7d70280d
jakathornsorv <jakathorn.sorv@mail.kmutt.ac.th>
2024-05-19 00:07:19 +0700

    done

--- fronted/src/libs/toDate.js
+++ fronted/src/libs/toDate.js
@@ -1,16 +1,16 @@
 export function toDate(dateTimeString) {
-      const [datePart, timePart] = dateTimeString.split("T")
-      const [year, month, day] = datePart.split("-")
+      const [datePart, timePart] = dateTimeString?.split("T")
+      const [year, month, day] = datePart?.split("-")
       
       let [hour, minute, second] = [0, 0, 0]
       if (timePart) {
-          [hour, minute, second] = timePart.replace("Z", "").split(":")
+          [hour, minute, second] = timePart?.replace("Z", "")?.split(":")
       }
       
       const event = new Date(Date.UTC(year, month - 1, day, hour, minute, second))
       
-      const formattedDate = event.toLocaleDateString('en-GB')
-      const formattedTime = event.toLocaleTimeString('en-GB')
+      const formattedDate = event?.toLocaleDateString('en-GB')
+      const formattedTime = event?.toLocaleTimeString('en-GB')
   
       return `${formattedDate} ${formattedTime}`
   }
