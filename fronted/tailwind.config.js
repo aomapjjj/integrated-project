e5005baf4e65d194b2b4ff87035bf44fcdef5cb0
pearmai <pearmai.wann@kmutt.ac.th>
2024-08-15 20:51:02 +0700

    PBI15-frontend-UI

--- fronted/tailwind.config.js
+++ fronted/tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         customPink: '#F785B1',
         customBlue: '#9FC3E9',
-      },
-    },
+        customPurple: '#9391E4'
+      }
+    }
   },
-  plugins: [require("daisyui")],
-  darkMode: "false",
+  plugins: [
+    require('daisyui'),
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadowCustom': {
+          textShadow: '2px 2px 3px rgba(0, 0, 0, 0.2)'
+        }
+      }
+      addUtilities(newUtilities)
+    }
+  ],
+  darkMode: 'false',
   daisyui: {
-    darkTheme: false,
-  },
+    darkTheme: false
+  }
 }
 
