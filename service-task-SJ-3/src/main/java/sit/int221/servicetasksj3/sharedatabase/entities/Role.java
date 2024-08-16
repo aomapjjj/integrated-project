8258d06c7f485a5d17727c6f82f7d6705966e171
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-08-16 21:03:42 +0700

    Get Data From Share Database

--- service-task-SJ-3/src/main/java/sit/int221/servicetasksj3/sharedatabase/entities/Role.java
+++ service-task-SJ-3/src/main/java/sit/int221/servicetasksj3/sharedatabase/entities/Role.java
@@ -1,6 +1,5 @@
 package sit.int221.servicetasksj3.sharedatabase.entities;
 
 public enum Role {
-
     LECTURER ,  STAFF , STUDENT
 }
--- service-task-SJ-3/src/main/java/sit/int221/servicetasksj3/sharedatabase/repositories/UserRepository.java
+++ service-task-SJ-3/src/main/java/sit/int221/servicetasksj3/sharedatabase/repositories/UserRepository.java
@@ -1,9 +1,8 @@
 package sit.int221.servicetasksj3.sharedatabase.repositories;
 
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int221.servicetasksj3.sharedatabase.entities.UsersEntity;
+import sit.int221.servicetasksj3.sharedatabase.entities.Users;
 
-public interface UserRepository extends JpaRepository<UsersEntity, String> {
+public interface UserRepository extends JpaRepository<Users, String> {
 
 }
