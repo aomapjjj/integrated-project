69795442b0d3fd75ef7b563bb4d940ea34ffbc39
jakathornsorv <jakathorn.sorv@mail.kmutt.ac.th>
2024-09-15 18:17:32 +0700

    last edit

--- fronted/src/views/ErrorPage.vue
+++ fronted/src/views/ErrorPage.vue
@@ -30,7 +30,7 @@ const goBackHome = () => {
   <div class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 text-center">
     <h1 class="text-base font-semibold" style="color: #f785b1">404</h1>
     <p class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl error-title">Page not found</p>
-    <p class="mt-6 text-base leading-7 text-gray-600">The requested Board does not exist</p>
+    <p class="mt-6 text-base leading-7 text-gray-600">There is a problem. Please try agian later.</p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
       <button @click="goBackHome" class="rounded-button rounded-md bg-error px-3.5 py-2.5 text-sm font-semibold text-white">Go back home</button>
     </div>
