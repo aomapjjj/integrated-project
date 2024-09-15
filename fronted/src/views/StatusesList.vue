6575625b96882ec702e4253dd31ff424db8a5046
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-09-15 23:44:24 +0700

    fix bug

--- fronted/src/views/StatusesList.vue
+++ fronted/src/views/StatusesList.vue
@@ -252,7 +252,7 @@ const deleteStatus = async (statusId) => {
       console.error(`Failed to delete item with ID ${statusId}`)
     }
 
-    if (status === 500) {
+    if (status === 401) {
       statusList.value = statusList.value.filter(
         (status) => status.id !== statusId
       )
