499d2dd0ae1295009479c1e3d3e899eb69af4c43
Panipak Jongsirivanich <panipak.jong@kmutt.ac.th>
2024-09-24 12:40:47 +0700

    Update BoardController.java

--- service-task-SJ-3/src/main/java/sit/int221/servicetasksj3/controller/BoardController.java
+++ service-task-SJ-3/src/main/java/sit/int221/servicetasksj3/controller/BoardController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v3/boards")
-@CrossOrigin(origins = { "http://localhost:5173/", "http://ip23sj3.sit.kmutt.ac.th", "http://intproj23.sit.kmutt.ac.th" } )
+@CrossOrigin(origins = { "http://localhost:5173/", "http://ip23sj3.sit.kmutt.ac.th", "http://intproj23.sit.kmutt.ac.th","https://intproj23.sit.kmutt.ac.th" } )
 
 public class BoardController {
     @Autowired
