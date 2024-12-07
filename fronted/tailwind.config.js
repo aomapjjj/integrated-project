9c5a57a47f4f3859644aca05bc8affc1ceb89277
pearmai <pearmai.wann@kmutt.ac.th>
2024-12-07 14:42:26 +0700

    config

--- fronted/tailwind.config.js
+++ fronted/tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        customPink: "#F785B1",
-        customBlue: "#9FC3E9",
-        customPurple: "#9391E4",
-        customRed: "#eb4343"
+        customPink: '#F785B1',
+        customBlue: '#9FC3E9',
+        customPurple: '#9391E4',
+        customRed: '#eb4343'
       }
     }
   },
   plugins: [
     require("daisyui"),
-    function ({ addUtilities }) {
-      const newUtilities = {
-        ".text-shadowCustom": {
-          textShadow: "2px 2px 3px rgba(0, 0, 0, 0.2)"
-        }
-      }
-      addUtilities(newUtilities)
-    }
+    // function ({ addUtilities }) {
+    //   const newUtilities = {
+    //     ".text-shadowCustom": {
+    //       textShadow: "2px 2px 3px rgba(0, 0, 0, 0.2)"
+    //     }
+    //   }
+    //   addUtilities(newUtilities)
+    // }
   ],
-  darkMode: "false",
+  darkMode: 'false',
   daisyui: {
     darkTheme: false
   }
